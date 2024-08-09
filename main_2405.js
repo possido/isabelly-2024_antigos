2fa48b03ce4deba27a319b23b826aa14cccad3d1
possido <134057973+possido@users.noreply.github.com>
2024-08-09 07:27:38 -0300

    Add files via upload

---
+++ main_2405.js
@@ -0,0 +1,9 @@
+document.querySelectorAll('.botao').forEach(btn => {
+    btn.addEventListener('click',function() {
+        document.querySelectorAll('.aba-conteudo').forEach(tab =>{
+            tab.classList.remove('ativo');
+        });
+        const index = Array.from(document.querySelectorAll('.botao')).indexOf(this);
+        document.querySelectorAll('.aba-conteudo')[index].classList.add('ativo');
+    });
+});
